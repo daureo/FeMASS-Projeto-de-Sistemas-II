8c1d33d7b9b58b82fa020b0245b416fa49ffe48e
Daureo Silva <daureocosta@gmail.com>
2023-03-03 11:51:11 -0300

    Codigo da aula de 02/03

---
+++ ClasseAbstrata2.java
@@ -0,0 +1,5 @@
+public abstract class ClasseAbstrata2 extends ClasseAbstrata {
+    public void metodo3(){
+        System.out.println("Olá, eu sou o método 3!");
+    }
+}
---
+++ HelloWorld.java
@@ -0,0 +1,17 @@
+
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+
+        ClasseConcreta NovaClasse = new ClasseConcreta();
+
+        NovaClasse.metodo1();
+        NovaClasse.metodo2();
+        NovaClasse.metodo3();
+
+    }
+
+    
+
+    
+}
